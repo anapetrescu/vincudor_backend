5bd5e34ba66bdf275a13c7a6876c3631b1de053c
Ana <anapetrescu011@gmial.com>
2019-04-01 00:19:36 +0300

    legatura BE FE

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const port = 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4201");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(require('./controllers/userController'))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
