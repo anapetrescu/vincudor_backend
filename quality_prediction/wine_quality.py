09c2586749676a24fa66a410199ef46f8ae3ee2b
John <john_giumanca@yahoo.com>
2019-04-08 15:05:34 +0300

    added ml module

---
+++ quality_prediction/wine_quality.py
@@ -0,0 +1,12 @@
+import pickle
+import sys
+import numpy as np
+
+features = np.array(sys.argv[1:],ndmin=2).astype(np.float)
+
+filename = 'rfc_model.pkl'
+with open(filename, 'rb') as handle:
+    model = pickle.load(handle)
+    prediction = model.predict(features)
+
+print(prediction)
